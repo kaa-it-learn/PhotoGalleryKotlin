e395ca5c988ba571fd80b8963b547f20af69fd3a
Andrey Kruglov <kruglov.it@gmail.com>
2018-07-05 11:50:50 +0300

    Chapter 27 is completed with Picasso

--- app/src/main/java/ru/tvhelp/akruglov/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/ru/tvhelp/akruglov/photogallery/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package ru.tvhelp.akruglov.photogallery
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
@@ -8,7 +9,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_photo_gallery.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
@@ -53,14 +56,19 @@ class PhotoGalleryFragment(): Fragment() {
     }
 
     private inner class PhotoHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val itemImageView = itemView.findViewById<ImageView>(R.id.itemImageView)
+
         fun bindPhoto(photo: Photo) {
-            (itemView as TextView).text = photo.toString()
+            Picasso.get().load(photo.url_s).into(itemImageView)
         }
     }
 
     private inner class PhotoAdapter(private val photos: List<Photo>): RecyclerView.Adapter<PhotoHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
-            return PhotoHolder(TextView(activity!!))
+            val inflater = LayoutInflater.from(activity!!)
+            val view = inflater.inflate(R.layout.gallery_item, parent, false)
+            return PhotoHolder(view)
         }
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
